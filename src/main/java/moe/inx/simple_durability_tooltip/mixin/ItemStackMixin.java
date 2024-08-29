45d0501d8aed3d0da2fe4c78f17f57d23c4b164b
Infinidoge <infinidoge@inx.moe>
2024-08-29 11:06:21 -0400

    update to 1.21

--- src/main/java/moe/inx/simple_durability_tooltip/mixin/ItemStackMixin.java
+++ src/main/java/moe/inx/simple_durability_tooltip/mixin/ItemStackMixin.java
@@ -10,11 +10,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import net.minecraft.client.item.TooltipContext;
+import com.llamalad7.mixinextras.sugar.Local;
+
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.item.TooltipConfig;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
 import net.minecraft.text.Text;
-import net.minecraft.text.component.TranslatableComponent;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
@@ -28,18 +30,15 @@ public abstract class ItemStackMixin {
 	@Shadow
 	abstract int getDamage();
 
-	@Inject(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/item/TooltipContext;isAdvanced()Z", ordinal = 2), locals = LocalCapture.CAPTURE_FAILSOFT)
-	public void simple_durability_tooltip$getTooltip(@Nullable PlayerEntity player, TooltipContext context,
-			CallbackInfoReturnable<List<Text>> ci, List<Text> list) {
-		if (!context.isAdvanced()) {
+	@Inject(
+		method = "getTooltip",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/item/TooltipConfig;shouldShowAdvancedDetails()Z", ordinal = 1, shift = At.Shift.BEFORE)
+	)
+	public void simple_durability_tooltip$getTooltip(Item.TooltipContext context, @Nullable PlayerEntity player, TooltipConfig config,
+			CallbackInfoReturnable<List<Text>> ci, @Local List<Text> list) {
+		if (!config.shouldShowAdvancedDetails()) {
 			if (this.isDamaged()) {
-				try {
-					list.add(Text.translatable("item.durability", this.getMaxDamage() - this.getDamage(),
-							this.getMaxDamage()));
-				} catch (NoSuchMethodError e) {
-					list.add((Text) new TranslatableComponent("item.durability", this.getMaxDamage() - this.getDamage(),
-							this.getMaxDamage()));
-				}
+				list.add(Text.translatable("item.durability",this.getMaxDamage() - this.getDamage(),this.getMaxDamage()));
 			}
 		}
 	}
